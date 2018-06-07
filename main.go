b0a64a70622a87b01369ff1fc9ee3143bc64cd11
Son Luong Ngoc (Lazada Group) <son.luongngoc@lazada.com>
2018-06-07 09:49:22 +0700

    Add dingtalk message integration

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/dghubble/sling"
-	"log"
 	"fmt"
+	"github.com/dghubble/sling"
+	"github.com/sluongng/dingbot"
+	"log"
 	"time"
 )
 
@@ -11,6 +12,7 @@ const (
 	ChototBaseUrl = "https://gateway.chotot.com"
 	AdListingPath = "/v1/public/ad-listing"
 	MaxPrice      = 7000000
+	AccessToken   = "123456"
 )
 
 var (
@@ -40,6 +42,7 @@ type AdPost struct {
 	Subject     string `json:"subject"`
 	Price       int64  `json:"price"`
 	IsCompanyAd bool   `json:"company_ad"`
+	ImageURL    string `json:"image"`
 }
 
 type Response struct {
@@ -52,8 +55,6 @@ func main() {
 	maxTime := time.Now().Truncate(1 * time.Hour)
 
 	for {
-		//log.Printf("MaxTime is %d", maxTime)
-
 		AdListingResult := new(Response)
 		_, err := sling.New().Get(ChototBaseUrl + AdListingPath).QueryStruct(Q1Params).ReceiveSuccess(AdListingResult)
 		if err != nil {
@@ -75,12 +76,51 @@ func main() {
 			fmt.Printf("Subject: %s \n", RentalPost.Subject)
 			fmt.Printf("Price: %d \n", RentalPost.Price)
 			fmt.Printf("*** \n")
+			Send2DingTalk(
+				RentalPost.AdID,
+				fmt.Sprintf("https://nha.chotot.com/%d.htm", RentalPost.ListID),
+				RentalPost.Subject,
+				RentalPost.Price,
+				RentalPost.ImageURL,
+			)
 
 			// TODO: Launch a chat webhook here.
 		}
 		maxTime = tempMaxTime
-		//fmt.Printf("New MaxTime is %d", maxTime)
 
 		time.Sleep(1 * time.Minute)
+	}
+}
+
+func Send2DingTalk(adID int32, url string, subject string, price int64, imageURL string) {
+	contentTemplate := `
+
+![](%s)
+
+**AdID**: %d
+
+**URL**: %s
+
+**Subject**: %s
+
+**Price**: %d
+`
+	content := fmt.Sprintf(contentTemplate, imageURL, adID, url, subject, price)
+
+	ChatBotService := dingbot.NewClient(AccessToken).RobotService
+	mdMessage := &dingbot.MarkdownMessage{
+		MsgType: "markdown",
+		Markdown: struct {
+			Title string `json:"title"`
+			Text  string `json:"text"`
+		}{
+			Title: "New Posting",
+			Text:  content,
+		},
+	}
+
+	err := ChatBotService.SendMarkdown(mdMessage)
+	if err != nil {
+		log.Printf("Something wrong with sending message to dingtalk: %s", err)
 	}
 }
